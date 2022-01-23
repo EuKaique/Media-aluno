b3fa329205e2b34bde44f0e5e5d7cb7caff95be0
Kaique <34290569+EuKaique@users.noreply.github.com>
2022-01-23 16:57:14 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 //Declaração das variáveis
-var contador = 0
-var notas = []
-var resultado
-
-//Alerta explicando ao usuário o que ele deve fazer
-alert("Digite as 4 notas do aluno")
-
-//Contador para o usuário digitar a nota
-while(contador <= 3){
-    notas.push(prompt("Digite uma nota"))
-    notas = notas.map(Number)
-    contador++
-}
+var contador = 0;
+var notas = [];
+var resultado;
 
 //Soma das notas do aluno com seu retorno
 function media(){   
-    let soma = 0
+    let soma = 0;
 
     for(let i in notas){
-        soma += notas[i]
+        soma += notas[i];
     }
-    return soma
+    return soma;
 }
+
 
 //Botão que recarrega a página
 document.getElementById("recarrega").addEventListener("click",()=>{
-    window.location.reload()
-})
+    window.location.reload();
+});
 
 //Função que carrega o script principal com a função média
-function carregaScript(){
-    resultado = document.getElementById("resposta")
+function fazMedia(){
+    //Ocultar botão e texto começar ao receber um clique
+    var comecar = document.querySelector('input');
+    var texto = document.querySelector('p');
+    if(comecar && texto){
+        comecar.style.display='none';
+        texto.style.display='none';
+    }
+    //Alerta explicando ao usuário o que ele deve fazer
+    alert("Digite as 4 notas do aluno");
 
-    media()
+
+    //Contador para o usuário digitar a nota
+    while(contador <= 3){
+        notas.push(prompt("Digite uma nota"));
+        notas = notas.map(Number);
+        contador++
+    }
+
+    resultado = document.getElementById("resposta");
+
+    media();
 
     //Dados que serão inseridos na Div resultado
     resultado.style.textAlign = "center"
@@ -42,10 +51,10 @@ function carregaScript(){
 
     //Condição que retorna uma mensagem de aprovado ou reprovado na Div resultado
     if((media()/notas.length) >= 7){
-        resultado.innerHTML += "<h3 style='color:#0f0'> Uhuuu! fui aprovado</h3>"
+        resultado.innerHTML += "<h3 style='color:#0E0'> Uhuuu! fui aprovado :D</h3>"
         resultado.innerHTML += "<img style='width: 100px' src='aprovado.png' alt='aprovado'>"
     }else{
-        resultado.innerHTML += "<h3 style='color:#f00'>Poxa fui reprovado :( </h3>"
+        resultado.innerHTML += "<h3 style='color:#E00'>Poxa fui reprovado :( </h3>"
         resultado.innerHTML += "<img style='width: 100px' src='reprovado.png' alt='reprovado'>"
     }
 }
